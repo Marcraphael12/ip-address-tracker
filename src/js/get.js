7bec7515d6e07b3648547dfe671042bf27602042
Marc raphael <marcraphael979@gmail.com>
2022-07-09 12:13:18 +0100

    import our DOM elements

---
+++ src/js/get.js
@@ -0,0 +1,11 @@
+// let's strat by importing our DOM elements
+
+// our form
+const submit = document.querySelector('.submit');
+const input = document.querySelector('.input');
+
+// our results
+const ip = document.querySelector('.ip-adress');
+const location = document.querySelector('.location');
+const timezone = document.querySelector('.timezone');
+const isp = document.querySelector('.isp');
