06ea103f8a86b474657609cdde494b25a4ab8a53
Marc raphael <marcraphael979@gmail.com>
2022-07-09 13:38:33 +0100

    get the input value

--- src/js/get.js
+++ src/js/get.js
@@ -9,3 +9,17 @@ const ip = document.querySelector('.ip-adress');
 const location = document.querySelector('.location');
 const timezone = document.querySelector('.timezone');
 const isp = document.querySelector('.isp');
+
+// we need first to get our input value
+const getInput = () => {
+  // we need to get the value of our input
+  const value = input.value;
+	console.log(value);
+	return value;
+}
+
+input.addEventListener('onkeyup', getInput);
+submit.addEventListener('click', (e)=> {
+	e.preventDefault();
+	ip.innerHTML=getInput();
+});
