0e909f8b4fbde1989115ca1fd9d64cc34c1a652a
Marc raphael <marcraphael979@gmail.com>
2022-07-09 13:09:51 +0100

    js/get: delete code

--- src/js/get.js
+++ src/js/get.js
@@ -15,26 +15,3 @@ const api_url = 'https://geo.ipify.org/api/v2/country?';
 const api_key = `apiKey=at_Yw8NM0CtrV4FB1v3PYflw1t3aigtm&`;
 const post_ip = `ipAddress=${input.value}`;
 
-// our functions
-// get the IP address
-const getIP = async (url) => {
-  // fetch the IP address to get a json object
-  const response = await fetch(url);
-  const data = await response.json();
-  return data;
-}
-
-// now we use an object to get the data we need
-const datas = {getIP};
-
-// receive the datas and display them
-const Display = async () => {
-  // receive the datas
-  datas.getIP(api_url + api_key + post_ip).then(data => {
-    // display the datas
-    ip.innerHTML = `${data.ipAddress}`;
-    location.innerHTML = `${data.location.region}, ${data.location.country}`;
-    timezone.innerHTML = `${data.location.timeZone}`;
-    isp.innerHTML = `${data.isp}`;
-  })
-}
