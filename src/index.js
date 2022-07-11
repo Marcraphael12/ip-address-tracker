4c5f805d3a064e2a8ea27291df243dc9eac18c71
Marc raphael <marcraphael979@gmail.com>
2022-07-11 08:29:09 +0100

    index: migrate code

--- src/index.js
+++ src/index.js
@@ -1,2 +1,59 @@
 import _ from 'lodash';
 import './style.css';
+
+// let's strat by importing our DOM elements
+
+// our form
+const submit = document.querySelector('.submit');
+const input = document.querySelector('.input');
+
+// our results
+const ip = document.querySelector('.ip-adress');
+const location = document.querySelector('.location');
+const timezone = document.querySelector('.timezone');
+const isp = document.querySelector('.isp');
+
+// we need first to get our input value
+const getInput = () => {
+  // we need to get the value of our input
+  return input.value.toString();
+}
+const value = getInput();
+
+input.addEventListener('onkeyup', (e)=> {
+  e.preventDefault();
+  getInput();
+});
+
+// our API
+const api_url = `https://geo.ipify.org/api/v2/country?apiKey=at_Yw8NM0CtrV4FB1v3PYflw1t3aigtm&ipAddress=${value}`;
+
+// we fethch our API
+const fetchData = async (url) => {
+  const response = await fetch(url);
+  const data = await response.json();
+  return data;
+}
+
+// we keep our data in an object
+const data = {fetchData};
+
+// we receive and display our data
+const displayData = async () => {
+  // an event listener on our submit button
+  // we receive our data
+
+    fetchData(api_url).then(data => {
+      // we display our data
+      ip.innerHTML = `${data.ip}`;
+      location.innerHTML = `${data.location.region}, ${data.location.country}`;
+      timezone.innerHTML = data.location.timezone;
+      isp.innerHTML = data.isp;
+    });
+}
+
+submit.addEventListener('click', (e)=> {
+  e.preventDefault();
+  console.log(getInput());
+  displayData();
+});
