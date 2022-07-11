d2fdb7dc04a90d703196ee2a6d3168f2bb04abe2
Marc raphael <marcraphael979@gmail.com>
2022-07-11 09:36:10 +0100

    import map container

--- src/js/get.js
+++ src/js/get.js
@@ -9,7 +9,9 @@ const ip = document.querySelector('.ip-adress');
 const location = document.querySelector('.location');
 const timezone = document.querySelector('.timezone');
 const isp = document.querySelector('.isp');
+const map_conatiner = document.querySelector('.map');
 
 // our API
 const key = 'at_Yw8NM0CtrV4FB1v3PYflw1t3aigtm';
 const API = `https://geo.ipify.org/api/v2/country?apiKey=${key}&ipAddress=`
+
